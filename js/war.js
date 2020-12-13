f75b0670308c0964057431c8c7f7bdc9e7a51679
mike <michael@hotmail.fr>
2020-12-14 00:54:56 +0100

    implementation des premiers combats

--- js/war.js
+++ js/war.js
@@ -6,28 +6,103 @@
 
 //when intuitive building, the druid checks : if there are no opponents. If so, go there.
 
-
-
-
 //send druidkillers
 
+ 
 
 
-function launchDruidAssault(){
+  function launchDruidAssault(){
 
+    let character;
+
+    let humanDruid = document.getElementById('human_druid');
+    
+
+    for(dkIndex = 0; dkIndex < 1; dkIndex++){
+       
+      character = getCharacterDomElement(AI, AI.druidKillers[dkIndex]);
+      
+      let characterLeft = character.offsetLeft;
+
+      //the character right value needs to go down until it's close to this value : then, it disappears
+      let moveInterval;
+   
+      if( characterLeft > humanDruid.offsetLeft ){
+   
+        alert('go!!');
+         moveInterval = setInterval(function(){ 
+         console.log('movement...');
+         characterLeft -= 3;
+         character.style.left = characterLeft + 'px';
+         console.log(characterLeft);
+   
+      if( (character.offsetLeft - humanDruid.offsetLeft) < 50){   
+
+
+        alert('the druid killers are attacking the druid!!!');
+
+   
+          clearInterval(moveInterval);
+   
+     }
+   
+    } , 20 );
+   
+
+   
+   
+   
+   } else {
+   
+   
+   moveInterval = setInterval(
+   
+   
+   function(){ 
+   
+   console.log('movement...');
+
+  
+    characterLeft += 3;
+   
+    character.style.left = characterLeft + 'px';
+   
+   
+    console.log(characterLeft);
+   
+   
+    if( (character.offsetLeft - humanDruid.offsetLeft) > 50){
+            
+          character.style.opacity = 0;
+       
+         setTimeout(function(){
+   
+           alert('the harvester collected 3 stones');
+   
+           stoneCollected();
+   
+            character.style.opacity = 1;
+   
+   
+            }, 2000);
+   
+           clearInterval(moveInterval);
+   
+        }
+   
+   
+      } , 20 );
+   
+   
+     }
+   
+   
+   
+    //end of druid killers loop   
+
+    }
 
 
 }
 
 
-function attackArmy(){
-
-
-}
-
-
-
-function move(){
-
-    //The ground is actually, hundreds of stacked divs
-}
